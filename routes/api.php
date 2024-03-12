37757b4e3a6d1a4e4f0aba769b9e6a97fc88204c
sokmeta <sokmeta.reth@quad.co.jp>
2024-03-12 15:29:58 +0700

    modify: set product apis under auth sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,29 +2,25 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
-use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('/products', ProductController::class);
 
+// User Auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// Product
+Route::get('/products', [ProductController::class, 'show']);
+Route::get('/products/{id}', [ProductController::class, 'index']);
+
 Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    
     Route::post('/logout', [AuthController::class, 'logout']);
 });
